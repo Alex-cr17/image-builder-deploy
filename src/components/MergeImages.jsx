00d736c652fab15bdec3c6cd39a73d628eeb11b9
alex.repryntsev <alex.repryntsev@gmail.com>
2023-07-31 12:10:57 +0300

    1. Add list of images on the side (if possible with small preview) and with delete and move-to-front buttons

--- src/components/MergeImages.jsx
+++ src/components/MergeImages.jsx
@@ -6,8 +6,76 @@ import classes from './ImageEditor.module.css';
 import ListItem from '@mui/material/ListItem';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { v4 as uuidv4 } from 'uuid';
 
-const ImageShape = ({ onSelect, image, onBringToFront, isSelected }) => {
+function blobToBase64(blob) {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+}
+
+const cropTransparentPixels = async (blob) => {
+  try {
+    const bitmap = await createImageBitmap(blob);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const { width, height } = bitmap;
+
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(bitmap, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const { data } = imageData;
+
+    let minX = width;
+    let minY = height;
+    let maxX = 0;
+    let maxY = 0;
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const alpha = data[(y * width + x) * 4 + 3];
+        if (alpha !== 0) {
+          minX = Math.min(minX, x);
+          minY = Math.min(minY, y);
+          maxX = Math.max(maxX, x);
+          maxY = Math.max(maxY, y);
+        }
+      }
+    }
+
+    const croppedWidth = maxX - minX + 1;
+    const croppedHeight = maxY - minY + 1;
+
+    const croppedCanvas = document.createElement('canvas');
+    const croppedCtx = croppedCanvas.getContext('2d');
+    croppedCanvas.width = croppedWidth;
+    croppedCanvas.height = croppedHeight;
+
+    croppedCtx.drawImage(canvas, minX, minY, croppedWidth, croppedHeight, 0, 0, croppedWidth, croppedHeight);
+
+    const croppedBlob = await new Promise((resolve) =>
+      croppedCanvas.toBlob(resolve, 'image/png')
+    );
+
+    return croppedBlob;
+  } catch (error) {
+    console.error('Error cropping transparent pixels:', error);
+    throw error;
+  }
+};
+
+
+const ImageShape = ({ onSelect, image, isSelected }) => {
   const shapeRef = useRef(null);
   const trRef = useRef(null);
 
@@ -24,12 +92,11 @@ const ImageShape = ({ onSelect, image, onBringToFront, isSelected }) => {
         id={image.id}
         className={image.id}
         image={image.img}
+
         zIndex={image.zIndex}
-        onMouseDown={onBringToFront}
-        onTouchStart={onBringToFront}
         ref={shapeRef}
-        onClick={onSelect}
-        onTouchEnd={onSelect}
+        onMouseDown={onSelect}
+        onTouchStart={onSelect}
         draggable
       />
       {isSelected && (
@@ -42,27 +109,45 @@ const ImageShape = ({ onSelect, image, onBringToFront, isSelected }) => {
 }
 
 const ImageBuilder = () => {
-  const stageRef = useRef(null);
   const [images, setImages] = useState([]);
+  const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
+  const stageRef = useRef(null);
   const canvasWrapperRef = useRef(null);
 
+  useEffect(() => {
+    const updateCanvasSize = () => {
+      const { clientWidth, clientHeight } = canvasWrapperRef.current;
+      setCanvasSize({ width: clientWidth, height: clientHeight });
+    };
+
+    // Initial canvas size
+    updateCanvasSize();
+  }, []);
 
   const handleImageUpload = (e) => {
+
     const file = e.target.files[0];
     const reader = new FileReader();
 
     reader.onload = () => {
       const img = new window.Image();
       img.src = reader.result;
-
       img.onload = () => {
-        setImages([...images, { name: file.name, id: images.length + 1, src: reader.result, img }]);
+        cropTransparentPixels(img).then( async blobObject => {
+          const croppedImg = new window.Image();
+          const blobBase64 = await blobToBase64(blobObject)
+          croppedImg.src = blobBase64;
+
+          setImages([...images, { name: file.name, id: uuidv4(), src: URL.createObjectURL(blobObject), img: croppedImg }]);
+        })
       };
+
     };
 
     if (file) {
       reader.readAsDataURL(file);
     }
+    e.target.value = null
   };
 
   const handleSelect = (id) => {
@@ -76,19 +161,44 @@ const ImageBuilder = () => {
     });
   };
 
+
+
+  const getCanvasElementFromStage = stageRef => {
+    if (!stageRef || !stageRef.current) {
+      return null; // Return null or handle the case when the Stage ref is not available
+    }
+
+    // The stageRef.current contains the actual Konva Stage instance
+    const konvaStage = stageRef.current.getStage();
+
+    // Use the Konva Stage instance to get the canvas element
+    if (konvaStage && konvaStage.content) {
+      return konvaStage.content;
+    }
+
+    return null; // Return null or handle the case when the canvas element is not available
+  };
+
   const handleDownload = () => {
-    const dataURL = stageRef.current.toDataURL();
-    const link = document.createElement('a');
-    link.download = 'canvas_image.png';
-    link.href = dataURL;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+
+    setImages((prevImages) =>  prevImages.map(image => ({ ...image, isSelected: false })));
+
+    setTimeout(() => {
+
+      cropTransparentPixels(getCanvasElementFromStage(stageRef).firstChild).then(result => {
+        const link = document.createElement('a');
+        link.download = 'canvas_image.png';
+        link.href = URL.createObjectURL(result);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+
+    }, 0)
   };
 
   const handleBringToFront = (id) => {
     setImages((prevImages) => {
-      // Increase the zIndex of the selected image to bring it to the front
       const updatedImages = prevImages.map((image) => {
         if (image.id === id) {
           return { ...image, zIndex: Date.now() }; // Set a new zIndex to move it to the front
@@ -99,6 +209,17 @@ const ImageBuilder = () => {
     });
   };
 
+  const handleDeleteImage = (id) => {
+    setImages((prevImages) => {
+      // Increase the zIndex of the selected image to bring it to the front
+      const updatedImages = prevImages.filter((image) => {
+        if (image.id !== id) {
+          return image;
+        }
+      });
+      return updatedImages;
+    });
+  }
   const checkDeselect = (e) => {
     // deselect when clicked on empty area
     const clickedOnEmpty = e.target === e.target.getStage();
@@ -119,7 +240,7 @@ const ImageBuilder = () => {
       <div style={{ display: "flex", width: "100%", justifyContent: "space-between", alignItems: "start" }}>
         <div ref={canvasWrapperRef} className={classes.canvasContainer}>
           <Stage
-            width="800"
+            width={canvasSize.width}
             height="500"
             onMouseDown={checkDeselect}
             onTouchStart={checkDeselect}
@@ -131,8 +252,7 @@ const ImageBuilder = () => {
                   onSelect={() => handleSelect(image.id)}
                   image={image}
                   isSelected={image.isSelected}
-                  onBringToFront={() => handleBringToFront(image.id)}
-                  key={image.name}
+                  key={image.id}
                 />
               ))}
             </Layer>
@@ -152,10 +272,28 @@ const ImageBuilder = () => {
                 <input type="file" hidden accept=".png,.jpg,.jpeg" />
               </Button>
             </ListItem>
-            {images.length ? images.map((file, index) => {
+            {images.length ? images.map((data, index) => {
               return (
-                <ListItem  key={index} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                  <div>{file.name}</div>
+                <ListItem
+                  key={index}
+
+                  secondaryAction={
+                    <>
+                      <IconButton edge="end" aria-label="bring to front" onClick={() => handleBringToFront(data.id)}>
+                        <ArrowUpwardIcon />
+                      </IconButton>
+                      <IconButton sx={{ marginLeft: "20px" }} edge="end" aria-label="delete" onClick={() => handleDeleteImage(data.id)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar variant="square"  sx={{ bgcolor: "transparent" }}>
+                      <img width="100%" height="100%" style={{ objectFit: 'contain' }} src={data.src} />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={data.name} />
                 </ListItem>
               )
             }) : ''}
